ed5b6b437b14fb7b8a7e0a7b01ebe736ed88c20a
sdf2685 <sdf2685@sasa.hs.kr>
2019-11-25 14:11:23 +0900

    미완성, 일단 1인용 게임으로 클래스 구현

--- example.py
+++ example.py
@@ -4,6 +4,84 @@ import time
 pre_time=time.time()
 TEXTCOLOR = (255, 255, 255)
 COOLTIME_LIMIT=5
+class Mundo:
+    def __init__(self,x,y):
+        self.health=100
+        self.strength=100
+        self.armor=100
+        self.healthregen=0.02
+        self.cooltime_limit=5
+        self.arrows=[]
+        self.x=x
+        self.y=y
+
+    def move(self,k0,k1,k2,k3):
+        if k0:
+            if 23 <= self.y - 5 <= 240 - 23:
+                self.y = self.y - 5
+        elif k2:
+            if 23 <= self.y + 5 <= 240 - 23:
+                self.y = self.y + 5
+        elif k1:
+            if 32 <= self.x - 5 <= 640 - 32:
+                self.x = self.x - 5
+        elif k3:
+            if 32 <= self.x + 5 <= 640 - 32:
+                self.x = self.x + 5
+
+    def attack(self,time):
+        position = pygame.mouse.get_pos()
+        time = time.time();
+        self.arrows.append([math.atan2(position[1] - (playpos[1] + 32), position[0] - (playpos[0] + 26)), \
+                       playpos[0] + 26, playpos[1] + 32])
+        return time
+    def injured(self,oppomundo):
+        for bullet in oppomundo.arrows:
+            bullrect=pygame.Rect(arrow.get_rect())
+            bullrect.left=bullet[1]
+            bullrect.right=bullet[2]
+            tmpmundo=mundo.get_rect(bottomright=(self.x,self.y))
+            if tmpmundo.colliderect(bullrect):
+                self.health-=oppomundo.strength
+                oppomundo.arrows.pop()
+
+Mundo1=Mundo(320,100)
+Mundo2=Mundo(320.500)
+def basicscreenblit():
+    screen.fill((0, 0, 0))  # R,G,B
+    for x in range(width // grass.get_width() + 1):
+        for y in range(height // grass.get_height() + 1):
+            screen.blit(grass, (x * 100, y * 100))
+    screen.blit(bush, (0, 30))
+    screen.blit(bush, (0, 135))
+    screen.blit(bush, (0, 240))
+    screen.blit(bush, (0, 345))
+    screen.blit(bush, (540, 30))
+    screen.blit(bush, (540, 135))
+    screen.blit(bush, (540, 240))
+    screen.blit(bush, (540, 345))
+    for x in range(width // wall.get_width() + 1):
+        screen.blit(wall, (x * 40, 240))
+
+def mundoblit(tmpmundo,pos):
+    angle = math.atan2(pos[1] - (tmpmundo.x + 32), pos[0] - (tmpmundo.y + 26))
+    playerrot = pygame.transform.rotate(mundo, 360 - angle * 57.29)
+    playerpos1 = (tmpmundo.x - playerrot.get_rect().width // 2, tmpmundo.y - playerrot.get_rect().height // 2)
+    screen.blit(playerrot, playerpos1)
+
+def arrowblit(tmpmundo):
+    for bullet in tmpmundo.arrows:
+        index=0
+        velx=math.cos(bullet[0])*10
+        vely=math.sin(bullet[0])*10
+        bullet[1]+=velx
+        bullet[2]+=vely
+        if bullet[1]<-64 or bullet[1]>640 or bullet[2]<-64 or bullet[2]>480:
+            tmpmundo.arrows.pop(index)
+        index=index+1
+    for projectile in tmpmundo.arrows:
+        arrow1=pygame.transform.rotate(arrow,360-projectile[0]*57.29)
+        screen.blit(arrow1,(projectile[1],projectile[2]))
 def wait_for_key_pressed():
     while True:
         for event in pygame.event.get():
@@ -27,13 +105,11 @@ def exit_game(self):
 # 8번까지 구현함
 pygame.init()
 width,height=640,480
-acc=[0,0]
-arrows=[]
 window = pygame.display.set_mode((width,height))
 background=pygame.image.load("resources/images/background.jpg")
 window.blit(background, (0, 0))
 font0=pygame.font.SysFont("Agency FB",36)
-font1 = pygame.font.SysFont("Liberation Serif", 24)
+font1=pygame.font.SysFont("Liberation Serif", 24)
 draw_text("MUNDO DODGEBALL",font0, window, (width / 3), (height / 3) + 100)
 draw_text("press annie key to start!",font1, window, (width / 3), (height / 3) + 150)
 draw_text("2019 OOP project by team MUNDO",font1, window, (width/3)*2-100, (height / 3)*2+100)
@@ -49,39 +125,13 @@ wall=pygame.image.load("resources/images/wall.png")
 keys=[False,False,False,False]
 playpos=[100,100]
 while True:
-    screen.fill((0,0,0)) # R,G,B
-    for x in range(width//grass.get_width()+1):
-        for y in range(height//grass.get_height()+1):
-            screen.blit(grass,(x*100,y*100))
-    screen.blit(bush, (0, 30))
-    screen.blit(bush, (0, 135))
-    screen.blit(bush, (0, 240))
-    screen.blit(bush, (0, 345))
-    screen.blit(bush, (540, 30))
-    screen.blit(bush, (540, 135))
-    screen.blit(bush, (540, 240))
-    screen.blit(bush, (540, 345))
-    for x in range(width//wall.get_width()+1):
-        screen.blit(wall,(x*40,240))
+    basicscreenblit()
     position=pygame.mouse.get_pos()
-    angle=math.atan2(position[1]-(playpos[1]+32),position[0]-(playpos[0]+26))
-    playerrot=pygame.transform.rotate(mundo, 360 - angle * 57.29)
-    playerpos1=(playpos[0]-playerrot.get_rect().width//2,playpos[1]-playerrot.get_rect().height//2)
-    screen.blit(playerrot,playerpos1)
-    for bullet in arrows:
-        index=0
-        velx=math.cos(bullet[0])*10
-        vely=math.sin(bullet[0])*10
-        bullet[1]+=velx
-        bullet[2]+=vely
-        if bullet[1]<-64 or bullet[1]>640 or bullet[2]<-64 or bullet[2]>480:
-            arrows.pop(index)
-        index=index+1
-    for projectile in arrows:
-        arrow1=pygame.transform.rotate(arrow,360-projectile[0]*57.29)
-        screen.blit(arrow1,(projectile[1],projectile[2]))
+    mundoblit(Mundo1,position)
+    mundoblit(Mundo2,position)
+    arrowblit(Mundo1)
+    arrowblit(Mundo2)
     pygame.display.flip()
-
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
@@ -95,12 +145,8 @@ while True:
                 keys[2]=True
             elif event.key==pygame.K_d:
                 keys[3]=True
-            elif event.key == pygame.K_SPACE and time.time()-pre_time>=COOLTIME_LIMIT:
-                position = pygame.mouse.get_pos()
-                pre_time=time.time();
-                acc[1] = acc[1] + 1
-                arrows.append([math.atan2(position[1] - (playpos[1] + 32), position[0] - (playpos[0] + 26)), \
-                playpos[0] + 26, playpos[1] + 32])
+            elif event.key == pygame.K_SPACE and time.time()-pre_time>=Mundo1.cooltime_limit:
+                pre_time=Mundo1.attack(pre_time)
         if event.type==pygame.KEYUP:
             if event.key==pygame.K_w:
                 keys[0]=False
@@ -110,16 +156,5 @@ while True:
                 keys[2]=False
             elif event.key==pygame.K_d:
                 keys[3]=False
-    if keys[0]:
-        if 23<=playpos[1]-5<=240-23:
-            playpos[1]=playpos[1]-5
-    elif keys[2]:
-        if 23 <= playpos[1] + 5 <= 240-23:
-            playpos[1]=playpos[1]+5
-    elif keys[1]:
-        if 32 <= playpos[0] - 5 <= 640-32:
-            playpos[0]=playpos[0]-5
-    elif keys[3]:
-        if 32 <= playpos[0] + 5 <= 640-32:
-            playpos[0]=playpos[0]+5
+    Mundo1.move(keys[0],keys[1],keys[2],keys[3])
 
